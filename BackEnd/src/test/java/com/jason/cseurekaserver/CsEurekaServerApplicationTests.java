fd0e49a9358ac510ad8cdd698e5c8e11e7d240a6
Jason Lee <jasdlee@outlook.com>
2020-04-12 22:45:56 +0800

    init

---
+++ BackEnd/src/test/java/com/jason/cseurekaserver/CsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jason.cseurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
