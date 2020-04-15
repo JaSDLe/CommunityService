d6fe456be8e9e8e1afe731df11b5c9abca165770
Jason Lee <jasdlee@outlook.com>
2020-04-15 23:50:02 +0800

    init webview

--- FrontEnd/Android/app/src/main/java/com/jason/MainActivity.java
+++ FrontEnd/Android/app/src/main/java/com/jason/MainActivity.java
@@ -3,6 +3,8 @@ package com.jason;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //获得控件
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        //访问网页
+        webView.loadUrl("http://192.168.0.101:8080/#/news/list");
+        //系统默认会通过手机浏览器打开网页，为了能够直接通过WebView显示网页，则必须设置
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                //使用WebView加载显示url
+                view.loadUrl(url);
+                //返回true
+                return true;
+            }
+        });
     }
 }
