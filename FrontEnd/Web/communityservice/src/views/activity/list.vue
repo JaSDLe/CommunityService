17d70fea595fe9cae08b0066c70ec11161cebad0
LiZisong <jasdlee@outlook.com>
2020-04-15 20:29:57 +0800

    add list

--- FrontEnd/Web/communityservice/src/views/activity/list.vue
+++ FrontEnd/Web/communityservice/src/views/activity/list.vue
@@ -1,15 +1,33 @@
 <template>
   <div>
+    <van-skeleton title avatar :row="3" :loading="loading">
+      <div>实际内容</div>
+    </van-skeleton>
+
     <tabbar />
   </div>
 </template>
 
 <script>
 import tabbar from "@/components/tab-bar";
+import { Skeleton } from "vant";
 
 export default {
   components: {
-    tabbar
+    tabbar,
+    [Skeleton.name]: Skeleton
+  },
+
+  data() {
+    return {
+      loading: true
+    };
+  },
+
+  mounted() {
+    setTimeout(() => {
+      // this.loading = false;
+    }, 1000);
   }
 };
 </script>
--- FrontEnd/Web/communityservice/src/views/news/list.vue
+++ FrontEnd/Web/communityservice/src/views/news/list.vue
@@ -1,15 +1,71 @@
 <template>
   <div>
+    <van-pull-refresh v-model="isRefreshing" @refresh="onRefresh" success-text="刷新成功">
+      <van-list
+        v-model="isListLoading"
+        :finished="isListFinished"
+        :error.sync="isListError"
+        @load="onLoad"
+        finished-text="没有更多了"
+        error-text="请求失败，点击重新加载"
+      >
+        <van-cell v-for="item in list" :key="item" :title="item" />
+      </van-list>
+    </van-pull-refresh>
+
     <tabbar />
   </div>
 </template>
 
 <script>
 import tabbar from "@/components/tab-bar";
+import { PullRefresh, List, Cell } from "vant";
 
 export default {
   components: {
-    tabbar
+    tabbar,
+    [PullRefresh.name]: PullRefresh,
+    [List.name]: List,
+    [Cell.name]: Cell
+  },
+
+  data() {
+    return {
+      list: [],
+      isRefreshing: false,
+      isListLoading: false,
+      isListFinished: false,
+      isListError: false
+    };
+  },
+
+  methods: {
+    onLoad() {
+      setTimeout(() => {
+        if (this.isRefreshing) {
+          this.list = [];
+          this.isRefreshing = false;
+        }
+
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1);
+        }
+        this.isListLoading = false;
+
+        if (this.list.length >= 40) {
+          this.isListFinished = true;
+        }
+      }, 1000);
+    },
+    onRefresh() {
+      // 清空列表数据
+      this.isListFinished = false;
+
+      // 重新加载数据
+      // 将 loading 设置为 true，表示处于加载状态
+      this.isListLoading = true;
+      this.onLoad();
+    }
   }
 };
 </script>
