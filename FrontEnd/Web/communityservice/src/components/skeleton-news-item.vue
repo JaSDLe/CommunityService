7d7afa6cf30b965189688cdc23657984523700f1
LiZisong <jasdlee@outlook.com>
2020-05-07 18:29:52 +0800

    update skeleton

--- FrontEnd/Web/communityservice/src/components/skeleton-news-item.vue
+++ FrontEnd/Web/communityservice/src/components/skeleton-news-item.vue
@@ -5,7 +5,7 @@
     avatar
     avatar-shape="square"
     :row="2"
-    :row-width="[80, 50]"
+    :row-width="['30%', '20%']"
     :loading="loading"
   >
     <news-item
@@ -39,14 +39,25 @@ export default {
 /deep/.van-skeleton {
   min-height: 65px;
 }
-/deep/.van-skeleton__content {
-  padding-right: 16px;
-}
 /deep/.van-skeleton__avatar {
   width: 85px !important;
   height: 65px !important;
   border-radius: 5px;
   order: 1;
   margin-right: 0;
+}
+/deep/.van-skeleton__content {
+  padding-right: 16px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+/deep/.van-skeleton__content div:first-child {
+  margin-top: 0;
+  background-color: green;
+}
+/deep/.van-skeleton__content div:last-child {
+  margin-top: 0;
+  background-color: red;
 }
 </style>
