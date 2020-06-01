9c28b55cc804f108a23f687792bba0afe6d56acc
LiZisong <jasdlee@outlook.com>
2020-06-01 10:54:25 +0800

    update message

---
+++ FrontEnd/Web/communityservice/src/views/message/detail.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <nav-bar title="" />
+
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/nav-bar"
+import { } from "vant"
+import { findByMessageId } from "@/api/message"
+
+export default {
+  components: {
+    NavBar,
+  },
+
+  props: {},
+
+  data() {
+    return {
+      messsageId: null,
+      data: {}
+    }
+  },
+
+  computed: {},
+
+  watch: {},
+
+  created() {
+    this.messsageId = this.$route.query.messsageId
+    this.getData()
+  },
+
+  mounted() { },
+
+  methods: {
+    getData() {
+      let param = {
+        messsageId: this.messsageId,
+        operator: this.$store.getters.getAccountId
+      }
+      findByMessageId(param).then(res => {
+        if (res.data) {
+          this.data = res.data
+        }
+      })
+    }
+  }
+};
+</script>
+
+<style scoped lang='postcss'>
+</style>
--- FrontEnd/Web/communityservice/src/views/message/list.vue
+++ FrontEnd/Web/communityservice/src/views/message/list.vue
@@ -1,16 +1,31 @@
 <template>
   <div>
+    <div style="height: 50px;" />
+
     <tab-bar />
   </div>
 </template>
 
 <script>
 import TabBar from "@/components/tab-bar";
+import { pageMessage } from "@/api/message";
 
 export default {
   components: {
     TabBar
-  }
+  },
+
+  data() {
+    return {};
+  },
+
+  computed: {},
+
+  watch: {},
+
+  created() {},
+
+  methods: {}
 };
 </script>
 
